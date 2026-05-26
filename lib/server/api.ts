99dc2885fb73fa3d6a4ff47d33e83d23a58d1ff1
Tanish Tirpathi <tanishtirpathi0@gmail.com>
2026-05-27 01:37:16 +0530

    update next url

--- lib/server/api.ts
+++ lib/server/api.ts
@@ -1,38 +1,39 @@
-import { cookies } from "next/headers";
+  import { cookies } from "next/headers";
 
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
 
-export async function getMe() {
+  export async function getMe() {
 
-  const cookieStore = await cookies();
+    const cookieStore = await cookies();
 
-  const res = await fetch(`/api/me`, {
-    headers: {
-      Cookie: cookieStore.toString(),
-    },
-    cache: "no-store",
-  });
+    const res = await fetch(`${baseUrl}/api/me`, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+      cache: "no-store",
+    });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch user");
+    if (!res.ok) {
+      throw new Error("Failed to fetch user");
+    }
+
+    return res.json();
   }
 
-  return res.json();
-}
+  export async function allStudents() {
 
-export async function allStudents() {
+    const cookieStore = await cookies();
+    const response = await fetch(`${baseUrl}/api/allstudent`, {
+      method: "POST",
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+      cache: "no-store",
+    });
 
-  const cookieStore = await cookies();
-  const response = await fetch(`/api/allstudent`, {
-    method: "POST",
-    headers: {
-      Cookie: cookieStore.toString(),
-    },
-    cache: "no-store",
-  });
+    if (!response.ok) {
+      throw new Error("Failed to fetch students");
+    }
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch students");
+    return response.json();
   }
-
-  return response.json();
-}
