81317bb26019c0fcb8bda065ee590bef5f4c1a3a
Tanish Tirpathi <tanishtirpathi0@gmail.com>
2026-05-27 01:28:54 +0530

    fixing bugs again

--- app/teacher/dashboard/page.tsx
+++ app/teacher/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { getMe, allStudents } from "@/lib/server/api";
 import DashboardClient from "./dashboard-client";
-
+export const dynamic = "force-dynamic";
 export default async function DashboardPage() {
   
     const user = await getMe();
